e7158c9461006ba9b4647e2e5b26cc178ae1c313
Mark Brooks <Markabrooks@gmail.com>
2018-01-05 11:09:05 -0700

    Error on line 22 of Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component{
+  constructor(props) {
+    super(props);
+
+    const album = albumData.find( album => {
+      console.log(album);
+      return album.slug === this.props.match.params.slug
+    });
+
+    this.state = {
+      album: album
+    };
+  }
+
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album will go here
-      </section>
+         <section id="album-info">
+           <img id="album-cover-art" src={this.state.album.albumCover} />
+           <div className="album-details">
+             <h1 id="album-title">{this.state.album.title}</h1>
+             <h2 className="artist">{this.state.album.artist}</h2>
+             <div id="release-info">{this.state.album.releaseInfo}</div>
+           </div>
+         </section>
+       </section>
     );
   }
 }
-
 
 export default Album;
