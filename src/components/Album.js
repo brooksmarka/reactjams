3832996545a97729059406c9d829c0248164db31
Mark Brooks <Markabrooks@gmail.com>
2018-03-09 13:12:33 -0700

    Assignment 6 Album: Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -33,6 +33,23 @@ class Album extends Component{
                 <col id="song-duration-column" />
               </colgroup>
               <tbody>
+              <section className='songs'>
+                {
+                  this.state.album.songs.map( (song, index ) =>
+                  <tr className="song" key={index} >
+                    <td className="song-actions">
+                      <button>
+                        <span className="song-number">{index+1}</span>
+                        <span className="ion-play"></span>
+                        <span className="ion-pause"></span>
+                      </button>
+                    </td>
+                    <td className="song-title">{song.title}</td>
+                    <td className="song-duration">{song.duration}</td>
+                  </tr>
+                  )
+                }
+              </section>
               </tbody>
             </table>
        </section>
