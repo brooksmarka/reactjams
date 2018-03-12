e50d762d7f013aa95c301b03ecb0ef066e7ff0cb
Mark Brooks <Markabrooks@gmail.com>
2018-03-11 18:13:16 -0700

    Assignment 8: next button on player bar complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -47,8 +47,17 @@ class Album extends Component{
   }
 
   handlePrevClick(){
-    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song )
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
     const newIndex = Math.max(0, currentIndex -1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play(newSong);
+  }
+
+  handleNextClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song );
+    const newIndex = Math.min(this.state.album.songs.length - 1, currentIndex + 1);
+    console.log(newIndex);
     const newSong = this.state.album.songs[newIndex];
     this.setSong(newSong);
     this.play(newSong);
@@ -93,6 +102,7 @@ class Album extends Component{
               currentSong={this.state.currentSong}
               handleSongClick={() => this.handleSongClick(this.state.currentSong)}
               handlePrevClick={() => this.handlePrevClick()}
+              handleNextClick={() => this.handleNextClick()}
             />
        </section>
     );
