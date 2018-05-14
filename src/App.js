71e13305cc97f7b0e9202b2070def5e0a376ff5e
Mark Brooks <Markabrooks@gmail.com>
2018-05-14 12:35:43 -0600

    added sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
-import "./App.css";
 import Landing from "./components/Landing";
 import Library from "./components/Library";
 import Album from "./components/Album";
@@ -8,13 +7,16 @@ import Album from "./components/Album";
 class App extends Component {
   render() {
     return (
-      <div className="App bg-green black-70 pa4">
+      <div
+        className="App bg-dark-green
+       pa4"
+      >
         <header>
           <nav>
-            <Link className="black link dim" to="/">
+            <Link className="navy link dim" to="/">
               Landing
             </Link>
-            <Link className="black link dim" to="/library">
+            <Link className="green link dim" to="/library">
               Library
             </Link>
           </nav>
