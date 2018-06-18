8dbbddc183054daa5c0d125bf33870056760c0a4
Mark Brooks <Markabrooks@gmail.com>
2018-06-18 15:50:16 -0600

    created <Header /> component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
 import React, { Component } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import "./styles/main.css";
 import Landing from "./components/Landing";
 import Library from "./components/Library";
 import Album from "./components/Album";
+import Header from "./components/Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="header dt h3 bg-dark-purple w-100 border-box ph3 pv3 pv3-ns ph3-m ph3-l">
-          <nav className="nav dt fw6 w-100">
-            <Link
-              to="/"
-              title="Home"
-              className="dtc v-mid mid-gray link dim w-25 tl"
-            >
-              <img
-                src="./assets/images/rjlogo.png"
-                alt="Site Logo"
-                className="dib w3 h3"
-              />
-            </Link>
-            <div className="dtc v-mid w-75 tr">
-              <Link
-                to="/library"
-                className="av link turq dim dark-gray f5 f4-ns dib mr3 mr5-ns"
-              >
-                Library
-              </Link>
-            </div>
-          </nav>
-        </header>
-
+        <Header />
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => (
+  <header className="header dt h3 bg-dark-purple w-100 border-box ph3 pv3 pv3-ns ph3-m ph3-l">
+    <nav className="nav dt fw6 w-100">
+      <Link to="/" title="Home" className="dtc v-mid mid-gray link dim w-25 tl">
+        <img
+          src="./assets/images/rjlogo.png"
+          alt="Site Logo"
+          className="dib w3 h3"
+        />
+      </Link>
+      <div className="dtc v-mid w-75 tr">
+        <Link
+          to="/library"
+          className="av link turq dim dark-gray f5 f4-ns dib mr3 mr5-ns"
+        >
+          Library
+        </Link>
+      </div>
+    </nav>
+  </header>
+);
+
+export default Header;
